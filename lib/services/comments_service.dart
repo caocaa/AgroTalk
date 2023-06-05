9bbb2a84d470784c5d70983e3b9123243b7b141f
nurandin <andininurul83@gmail.com>
2023-06-05 22:47:02 +0700

    comments_service

---
+++ lib/services/comments_service.dart
@@ -0,0 +1,43 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import '../models/topic.dart';
+import 'package:http/http.dart' as http;
+
+String? id;
+String? id_user;
+String? id_groker;
+String? comment;
+
+//create comment
+Future createTopic(String comment) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  final String? action = prefs.getString('id');
+
+  try {
+    var url = "http://192.168.1.7:8000/api/groker/1/comments";
+    var body = {"comment": comment};
+    var token = prefs.getString('token')?.split("|")[1];
+    // return print(body);
+    var hasil = await http.post(Uri.parse(url), body: body, headers: {
+      "Accept": "Application/Json",
+      "Authorization": 'Bearer $token'
+    });
+    // print(hasil);
+    print(hasil.statusCode.toString());
+
+    if (hasil.statusCode != 200) {
+      print("error tambah komentar");
+      return hasil.statusCode.toString();
+    }
+
+    if (hasil.statusCode == 200) {
+      print("Komentar dibuat");
+      print(hasil.body);
+      id = topicModelFromJson(hasil.body).data.id;
+      comment = topicModelFromJson(hasil.body).data.nama_topik;
+
+      return comment;
+    }
+  } catch (e) {
+    print(e.toString());
+  }
+}
